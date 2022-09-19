4a52cb5458eb8d59f95a7bf46bb82dce3664d339
thomasRgl <thomasroglin@free.fr>
2022-09-19 15:00:51 +0200

    up

--- asus/.config/nvim/lua/mappings/init.lua
+++ asus/.config/nvim/lua/mappings/init.lua
@@ -1,7 +1,7 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-vim.g.mapleader = ' '
+vim.g.mapleader = '91'
 
 map('n', '<leader>p', ':NvimTreeToggle<cr>', opts)
 map('n', '<leader>e', ':help option-list<CR>', opts )
--- dwm/config.def.h
+++ dwm/config.def.h
@@ -73,12 +73,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[] = { "rofi", "-show", "run", NULL };
-static const char *termcmd[]  = { "st", NULL };
-
+static const char *termcmd[] = { "st", NULL };
+static const char *setkbfr[] = { "setxkbmap", "fr", NULL };
+static const char *setkbus[] = { "setxkbmap", "us", NULL };
 
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_q,      spawn,          {.v = setkbfr} },
+	{ MODKEY,                       XK_a,      spawn,          {.v = setkbus} },
 	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
